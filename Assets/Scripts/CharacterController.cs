021e66c9ad2679986a8ff7b92cccee330fe01557
Splexxxer <Marcel.schlipf@gmx.de>
2021-06-17 00:23:45 +0200

    Removed crap and addet scripts folder

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class oiuz : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
