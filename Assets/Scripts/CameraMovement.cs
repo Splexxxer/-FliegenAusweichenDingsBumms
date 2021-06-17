569b487ee589d1452852bce9eb589bee9bf011a5
Splexxxer <Marcel.schlipf@gmx.de>
2021-06-17 22:26:56 +0200

    Basic concept (not finished)
    Added PlayerController and Moving Cam also added sprite

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{   
+    public float speed = 3;
+    bool shouldStart = false;
+
+    
+    
+    void Start()
+    {
+        Invoke("turnShouldStartTrue", 2);
+        
+    }
+    
+    void Update()
+    {
+        if (shouldStart == true)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed,0);
+           
+        }
+    }
+    
+    void turnShouldStartTrue()
+    {
+        shouldStart = true;
+    }
+
+    void getFaster()//Have to be implemented //Maybe
+    {
+        
+    } 
+}
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 5f;
+    
     void Update()
     {
-        
+        float vHorizontal = Input.GetAxisRaw("HorizontalPlayer1");
+        float vVertical = Input.GetAxisRaw("VerticalPlayer1");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vHorizontal, vVertical) * speed;     
     }
 }
