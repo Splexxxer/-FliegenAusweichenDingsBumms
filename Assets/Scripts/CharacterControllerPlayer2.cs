79b200cc2eb29e0e19c4f2891a7005905e61a039
Splexxxer <Marcel.schlipf@gmx.de>
2021-06-18 22:59:06 +0200

    Addet Player 2

---
+++ Assets/Scripts/CharacterControllerPlayer2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerPlayer2 : MonoBehaviour
+{
+	public float speed = 10.0f;
+
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis("HorizontalPlayer2");
+		float moveVertical = Input.GetAxis("VerticalPlayer2");
+
+		Vector2 movment = new Vector2(moveHorizontal, moveVertical);
+
+		GetComponent<Rigidbody2D>().velocity = (movment * speed);
+	}
+}
