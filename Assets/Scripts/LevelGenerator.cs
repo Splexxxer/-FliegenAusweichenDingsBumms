ea0c95d2acf20b5600cdc8c8ecc40744bd59b289
KetYep <86020499+KetYep@users.noreply.github.com>
2021-06-30 00:02:37 +0200

    Level Generation vers.1

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform LevelPart_1;
+
+    private void Awake()
+    {
+        Transform LastLevelPartTransform;
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+        LastLevelPartTransform = SpawnLevelPart(LastLevelPartTransform.Find("EndPosition").position);
+    }
+
+    private Transform SpawnLevelPart(Vector2 Spawnposition)
+    {
+        Transform LevelPartTransform = Instantiate(LevelPart_1, Spawnposition, Quaternion.identity);
+        return LevelPartTransform;
+    }
+
+}
---
+++ Assets/Scripts/LevelGenerator2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator2 : MonoBehaviour
+{
+
+    public Gameobject position1;
+    public Gameobject position2;
+    public Gameobject position3;
+    public List<Gameobject> positions = new List<Gameobject>;
+
+
+    void Start()
+    {
+       
+        positions.Add(position1);
+        positions.Add(position2);
+        positions.Add(position3);
+        SpawnLevelPart();
+    }
+
+    private int SelectLevelPart()
+    {
+        int randomLevelPart = new Random.range(1,3);
+        return randomLevelPart;
+    }
+
+    void SpawnLevelPart()
+    {
+        foreach(positionX in positions)
+        {
+            Instantiate(Resource.Load("Assets/Prefabs/Wald/LevelPart_" + SelectLevelPart()), positionX.transform.position, Quanternion(0, 0, 0))
+        }
+    }
+
+
+}
